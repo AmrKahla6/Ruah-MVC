e82d9f206aa3872e3bf1d10457d085ba5f887a9e
AmrKahla6 <amrkahla6@gmail.com>
2020-12-13 21:13:18 +0200

    Builid View Class

---
+++ app/viewes/home/index.php
@@ -0,0 +1,8 @@
+<?php $this->siteTitle('Home'); ?>
+<?php $this->start('head'); ?>
+
+<?php $this->end(); ?>
+
+<?php $this->start('body'); ?>
+<h1>Welcome To home</h1>
+<?php $this->end(); ?>
--- config/config.php
+++ config/config.php
@@ -1,3 +1,7 @@
 <?php
     define('DEBUG' , true);
-    define('DEFAULT_CONTROLLER' , 'Home'); // Default Controller if is not define controller in url
+    define('DEFAULT_CONTROLLER' , 'Home');  // Default Controller if is not define controller in url
+    define('DEFAULT_LAYOUT'    , 'default'); //IF no layout is set in the controller use this layout
+
+    define('PROOT' , '/ruah/'); // Set this to '/' for live server
+    define('SITE_TITLE' , 'Ruah MVC Framework'); //This will be use if no site title
